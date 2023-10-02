0c25c3f8b2baf41fe2e553aae0aceaead8c396f8
Vikram Thakur <vikramthakur@Vikrams-MacBook-Pro.local>
2023-10-02 23:46:09 +0530

    Initial commit

---
+++ test/aws-cdk-ecs-fargate-load-balancer.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkEcsFargateLoadBalancer from '../lib/aws-cdk-ecs-fargate-load-balancer-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-ecs-fargate-load-balancer-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkEcsFargateLoadBalancer.AwsCdkEcsFargateLoadBalancerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
